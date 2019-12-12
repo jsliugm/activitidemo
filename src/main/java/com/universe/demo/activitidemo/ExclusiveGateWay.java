63598d98b8a005ba3d3afdc43c6ef014d450c527
liuguangming <jsliugm@gmail.com>
2019-12-12 23:50:22 +0800

    xxx

--- src/main/java/com/universe/demo/activitidemo/ExclusiveGateWay.java
+++ src/main/java/com/universe/demo/activitidemo/ExclusiveGateWay.java
@@ -1,7 +1,6 @@
 package com.universe.demo.activitidemo;
 
-import java.util.List;
-
+import lombok.extern.slf4j.Slf4j;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
@@ -10,17 +9,18 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
 @Slf4j
-public class HelloWorld {
+public class ExclusiveGateWay {
 	private ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 	/** 部署流程定义 */
 	@Test
 	public void deploymentProcessDefinition() {
 		Deployment deployment = processEngine.getRepositoryService()// 与流程定义和部署对象相关的Service
 				.createDeployment() // 创建一个部署对象
-				.name("helloworld入门程序")// 添加部署的名称
-				.addClasspathResource("diagrams/helloworld.bpmn")// 从classpath的资源中加载，一次只能加载一个文件
+				.name("ExclusiveGateWay")// 添加部署的名称
+				.addClasspathResource("diagrams/ExclusiveGateWay.bpmn")// 从classpath的资源中加载，一次只能加载一个文件
 				//.addClasspathResource("diagrams/helloworld.png")// 从classpath的资源中加载，一次只能加载一个文件
 				.deploy(); // 完成部署
 		System.out.println("部署ID:" + deployment.getId()); // 1
@@ -32,7 +32,7 @@ public class HelloWorld {
 	@Test
 	public void startProcessInstance() {
 		// 流程定义的key
-		String processDefinitionKey = "helloworld";
+		String processDefinitionKey = "baoxiao";
 		ProcessInstance pi = processEngine.getRuntimeService()// 与正在执行 的流程实例和执行对象相关的Service
 				.startProcessInstanceByKey(processDefinitionKey); // 使用流程定义的key启动流程实例,key对应helloworld.bpmn文件中id的属性值，使用key值启动，默认是按照最新版本的流程定义启动
 		System.out.println("流程实例ID:" + pi.getId());
